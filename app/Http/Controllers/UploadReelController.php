d4d629c8caf093043ed20b2b8162ac4c5600a879
Zeeshan Haidar <zeeshanhaidar@Zeeshans-MacBook-Pro.local>
2023-12-29 18:46:27 +1100

    Added php sdk code for uploading video as reel from graph api for instagram

--- app/Http/Controllers/UploadReelController.php
+++ app/Http/Controllers/UploadReelController.php
@@ -12,6 +12,10 @@ class UploadReelController extends Controller
         $request->validate([
             'video' => 'max:10240', // Assuming a maximum file size of 10 MB
         ]);
+        $config = array( // instantiation config params
+            'user_id' => '17841461509998509',
+            'access_token' => 'EABojrRw4lYoBO6tB2m5iG3YJ7mSeTZAMAAlpk4TbYQUG9ehQOk8SyZBLvKdHghZA9t8NKnvXrd90OJpwMo5XiTWUGrR0pUllawVGH0CYHOKnC693xNqPr9s5pnxdxkEUXbTtpoLZBZCarLxqOyFBWQ2rvngJZAL2rdHUuyd2F2CHhzwEo7WjRoHnSZBD8SyfW8kuy1aHnZBlungS38BX',
+        );
 
         $file = $request->file('video');
 
@@ -20,12 +24,31 @@ class UploadReelController extends Controller
         if($file->move('Uploads', $uniqueFileName))
         {
             $videoUrl = url('Uploads/' . $uniqueFileName);
-            dd($videoUrl);
-            // Print the URL or use it as needed
-            $apiService = new APIService();
-            $videoID = $apiService->graphAPIPostVideoToGetID($videoUrl);
-//            dd($videoID, $videoUrl);
-            $apiService->graphAPIPostVideoAsReel($videoID);
+            $media = new Media( $config );
+
+            $videoContainerParams = array( // container parameters for the video post
+                                'caption' => 'Another test', // caption for the post
+                                'video_url' => $videoUrl, // url to the video must be on a public server
+                                'media_type' => 'REELS', // specifying this should be set to VIDEO
+                                'is_carousel_item' => false, // is this in a carousel
+                'location_id' => '', // can be left blank otherwise the id of the facebook page associated with the location you want tagged
+                'thumb_offset' => 2 // number of milliseconds in the video to grab the thumbnail
+            );
+
+            // create video container
+            $videoContainer = $media->create( $videoContainerParams );
+
+            // get id of the video container
+            $videoContainerId = $videoContainer['id'];
+
+            dd($videoContainerId, $videoUrl);
+
+//            dd($videoUrl);
+//            // Print the URL or use it as needed
+//            $apiService = new APIService();
+//            $videoID = $apiService->graphAPIPostVideoToGetID($videoUrl);
+////            dd($videoID, $videoUrl);
+//            $apiService->graphAPIPostVideoAsReel($videoID);
 
         }
         else
