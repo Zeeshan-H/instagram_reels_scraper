e80682928659a9a480f9eddba42f7acbdd01f1e5
Zeeshan Haidar <zeeshanhaidar@Zeeshans-MacBook-Pro.local>
2023-12-30 00:30:20 +1100

    Increased sleep value

--- app/Http/Controllers/UploadReelController.php
+++ app/Http/Controllers/UploadReelController.php
@@ -25,7 +25,7 @@ class UploadReelController extends Controller
 
             $apiService = new APIService();
             $videoID = $apiService->graphAPIPostVideoToGetID($videoUrl, $caption);
-            sleep(10);
+            sleep(20);
             $result = $apiService->graphAPIPostVideoAsReel($videoID);
             if($result)
              return redirect()->back()->with('success', 'Reel has been uploaded');
