2589599524cff9a3cda70901d39484a78be9dd5d
Zeeshan Haidar <zeeshanhaidar@Zeeshans-MacBook-Pro.local>
2024-01-05 16:36:19 +1100

    dd video url 2

--- app/Http/Controllers/UploadReelController.php
+++ app/Http/Controllers/UploadReelController.php
@@ -6,7 +6,7 @@ use App\API\APIService;
 use FFMpeg\Format\Video\X264;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Validator;;
-//use FFMpeg\FFMpeg;
+use FFMpeg\FFMpeg;
 use RealRashid\SweetAlert\Facades\Alert;
 
 class UploadReelController extends Controller
@@ -33,6 +33,7 @@ class UploadReelController extends Controller
             $video->save($format, 'Uploads/ffmpeg-'. $uniqueFileName);
 
             $videoUrl2 = url('Uploads/ffmpeg-'. $uniqueFileName);
+            dd($videoUrl2);
 
             $apiService = new APIService();
             $videoID = $apiService->graphAPIPostVideoToGetID($videoUrl2, $caption);
