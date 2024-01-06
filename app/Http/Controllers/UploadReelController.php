19d7b701b70093f1dc51782aef69a4a219759e0b
Zeeshan Haidar <zeeshanhaidar@Zeeshans-MacBook-Pro.local>
2024-01-07 10:56:43 +1100

    FFMpeg fixed

--- app/Http/Controllers/UploadReelController.php
+++ app/Http/Controllers/UploadReelController.php
@@ -23,13 +23,22 @@ class UploadReelController extends Controller
         if($file->move('Uploads', $uniqueFileName))
         {
 
-            $videoUrl1 = url('Uploads/' . $uniqueFileName2);
 
             $ffmpegCommand = "ffmpeg -i \"$outputPath\" -c:v libx264 -profile:v high -level 4.2 -pix_fmt yuv420p -movflags +faststart -c:a aac -b:a 128k -ar 44100 -strict -2 -r 30 -s 640x1136 -t 15 -b:v 1500k -maxrate 1500k -bufsize 10240k -preset slow -crf 22 -f mp4 -y Uploads/\"$uniqueFileName2\"";
 
             exec($ffmpegCommand, $output, $returnCode);
 
             if ($returnCode === 0) {
+                $videoUrl1 = url('Uploads/' . $uniqueFileName2);
+                $apiService = new APIService();
+                $videoID = $apiService->graphAPIPostVideoToGetID($videoUrl1, $caption);
+                sleep(10);
+                $result = $apiService->graphAPIPostVideoAsReel($videoID);
+                if($result) {
+                    return response()->json(['success' => 'Video was successfully uploaded as a InstagramService reel.']);
+                }
+                else
+                    return response()->json(['error' => 'Video doesnt meet InstagramService reel requirements.']);
                 // Successfully executed FFmpeg command
                 echo "Video compressed and saved successfully.";
             } else {
@@ -65,15 +74,7 @@ class UploadReelController extends Controller
 //
 //            $videoUrl2 = url('Uploads/ffmpeg-'. $uniqueFileName);
 
-            $apiService = new APIService();
-            $videoID = $apiService->graphAPIPostVideoToGetID($videoUrl1, $caption);
-            sleep(10);
-            $result = $apiService->graphAPIPostVideoAsReel($videoID);
-            if($result) {
-                return response()->json(['success' => 'Video was successfully uploaded as a InstagramService reel.']);
-            }
-            else
-                return response()->json(['error' => 'Video doesnt meet InstagramService reel requirements.']);
+
         }
         return response()->json(['error' => 'An error occurred while uploading video as reel to InstagramService']);
     }
