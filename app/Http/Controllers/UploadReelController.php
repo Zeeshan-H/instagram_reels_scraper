d5a8cc274dcfad546afb83e47494ea18f39bf5c2
Zeeshan Haidar <zeeshanhaidar@Zeeshans-MacBook-Pro.local>
2024-01-07 10:13:30 +1100

    Added

--- app/Http/Controllers/UploadReelController.php
+++ app/Http/Controllers/UploadReelController.php
@@ -25,35 +25,35 @@ class UploadReelController extends Controller
             $videoUrl1 = url('Uploads/' . $uniqueFileName);
 
 //            // Use Laravel-FFMpeg to convert the video
-//            $ffmpeg = FFMpeg::create();
-//            $video = $ffmpeg->open($outputPath);
-//            $format = new X264();
-//            $format->setAudioCodec("aac");
-//            $format->setAudioKiloBitrate(128);
-//            $format->setAdditionalParameters([
-//                '-pix_fmt', 'yuv420p',
-//                '-profile:v', 'baseline',
-//                '-level', '3.0',
-//                '-movflags', '+faststart',
-//                '-b:v', '1500k',
-//            ]);
-//
-//// Video scaling
-//            $format->setAdditionalParameters(['-vf', 'scale=trunc(min(max(iw\,ih*dar)\,1920)/2)*2:trunc(min(max(iw/dar\,ih)\,1920)/2)*2']);
-//
-//            $format->setKiloBitrate(25000); // Maximum video bitrate in Kbps
-//            $format->setAudioKiloBitrate(128); // Audio bitrate in Kbps
-//            $format->setAudioChannels(2); // Stereo audio
-//
-//            $format->setAdditionalParameters(['-r', '30']); // Frame rate set to 30 FPS
-//            $format->setAdditionalParameters(['-t', '900']); // Maximum duration of 15 minutes (900 seconds)
-//
-//            $video->save($format, 'Uploads/ffmpeg-'. $uniqueFileName);
-//
-//            $videoUrl2 = url('Uploads/ffmpeg-'. $uniqueFileName);
+            $ffmpeg = FFMpeg::create();
+            $video = $ffmpeg->open($outputPath);
+            $format = new X264();
+            $format->setAudioCodec("aac");
+            $format->setAudioKiloBitrate(128);
+            $format->setAdditionalParameters([
+                '-pix_fmt', 'yuv420p',
+                '-profile:v', 'baseline',
+                '-level', '3.0',
+                '-movflags', '+faststart',
+                '-b:v', '1500k',
+            ]);
+
+// Video scaling
+            $format->setAdditionalParameters(['-vf', 'scale=trunc(min(max(iw\,ih*dar)\,1920)/2)*2:trunc(min(max(iw/dar\,ih)\,1920)/2)*2']);
+
+            $format->setKiloBitrate(25000); // Maximum video bitrate in Kbps
+            $format->setAudioKiloBitrate(128); // Audio bitrate in Kbps
+            $format->setAudioChannels(2); // Stereo audio
+
+            $format->setAdditionalParameters(['-r', '30']); // Frame rate set to 30 FPS
+            $format->setAdditionalParameters(['-t', '900']); // Maximum duration of 15 minutes (900 seconds)
+
+            $video->save($format, 'Uploads/ffmpeg-'. $uniqueFileName);
+
+            $videoUrl2 = url('Uploads/ffmpeg-'. $uniqueFileName);
 
             $apiService = new APIService();
-            $videoID = $apiService->graphAPIPostVideoToGetID($videoUrl1, $caption);
+            $videoID = $apiService->graphAPIPostVideoToGetID($videoUrl2, $caption);
             sleep(10);
             $result = $apiService->graphAPIPostVideoAsReel($videoID);
             if($result) {
