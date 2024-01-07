2d991b1cbe369c52e9d652131ef200d2703edbb2
Zeeshan Haidar <zeeshanhaidar@Zeeshans-MacBook-Pro.local>
2024-01-07 14:51:25 +1100

    Added

--- app/Http/Controllers/UploadReelController.php
+++ app/Http/Controllers/UploadReelController.php
@@ -31,12 +31,7 @@ class UploadReelController extends Controller
 
 //            // Use Laravel-FFMpeg to convert the video
 
-            $ffmpegCommand = "ffmpeg -i \"$outputPath\" " .
-                "-c:v libx264 -profile:v high -level 4.2 -pix_fmt yuv420p -movflags +faststart " .
-                "-c:a aac -b:a 128k -ar 44100 -strict -2 " .
-                "-vf \"scale=720:1280:force_original_aspect_ratio=decrease,pad=720:1280:(ow-iw)/2:(oh-ih)/2\" " .
-                "-r 30 -t 30 -b:v 1000k -maxrate 3500k -bufsize 10240k -preset slow -crf 22 " .
-                "-f mp4 -y output_instagram.mp4";
+            $ffmpegCommand = "ffmpeg -i \"$outputPath\" -c:v libx264 -preset slow -crf 22 -c:a aac -b:a 192k -movflags +faststart -y Uploads/ffmpeg-\"$uniqueFileName\"";
 
 
             exec($ffmpegCommand, $output, $returnCode);
